9cd45cb07048cdf9c84fe8f852078d9d3d3c6f15
AITOULAHYAN Fatima <fatima.aitoulahyan-etu@etu.univh2c.ma>
2026-08-22 12:48:21 +0100

    Update API URL in RagService

--- src/app/features/chatbot/service/rag.service.ts
+++ src/app/features/chatbot/service/rag.service.ts
@@ -13,12 +13,12 @@ export interface ChatMessage {
 })
 export class RagService {
   private http = inject(HttpClient);
-  private apiUrl = '/api';
+  private apiUrl = 'https://rag.stage.enset.top';
 
   sendMessage(question: string): Observable<any> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'X-API-Key': 'spelev-sec-key-zxvbnEE8YT65',
+      'X-API-Key': 'spelev-sec-key-zxvbnEE8YT65', 
     });
 
     return this.http.post<any>(`${this.apiUrl}/query`, { question }, { headers });
